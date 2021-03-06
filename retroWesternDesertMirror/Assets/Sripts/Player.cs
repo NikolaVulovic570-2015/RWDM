bcc3ca5979eb457486d1abfa279249a33bf166fd
Nikola Vulovic <nkl.vulovic96@gmail.com>
2021-03-06 20:27:56 +0100

    Pozadina animacije kretanja i kretanje

---
+++ retroWesternDesertMirror/Assets/Sripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float xInput, yInput;
+    private bool isMoving;
+    public float speed = 4f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isMoving = false;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+        isMoving = (xInput != 0 || yInput != 0);
+
+        if (isMoving)
+        {
+             var moveVector = new Vector3(xInput, yInput, 0);
+             rb.MovePosition(new Vector2((transform.position.x + moveVector.x * speed * Time.deltaTime),
+            (transform.position.y + moveVector.y * speed * Time.deltaTime)));
+            animator.SetFloat("Speed", xInput);
+        }
+    }
+    
+}
+
+
